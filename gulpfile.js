717341f39858b33ad1413ae4f99ee9811e9b58a3
Jack Harner <jack@harnerdesigns.com>
2018-06-18 20:15:32 -0600

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,84 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     sass = require('gulp-sass'),
-    header = require('gulp-header'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync'),
+    connect = require('gulp-connect'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    fs = require('fs');
-
-
-
-// ----- Package.Json -----
-
+    concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+var sourcemaps = require('gulp-sourcemaps');
 var package = require('./package.json');
+const zip = require('gulp-zip');
+const autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var bump = require('gulp-bump');
+var git = require('gulp-git');
 
-// ----- Environment Variables -----
 
-var scss_path = './assets/scss/style.scss';
-var js_watch = './assets/js/*.js';
-var scss_watch = './assets/scss/**/*.scss';
-var proxy = "localhost/blank2";
 
-// ----- CSS Banner -----
+
+gulp.task('log', function() { gutil.log("== MY LOG TASK ==") });
+
+
+gulp.task('sass', gulp.series(function(done) {
+    gulp.src('assets/scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ sourceComments: 'map' }))
+        .on('error', gutil.log)
+        .pipe(header(banner.theme, { package: package }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());;
+    done();
+}));
+
+gulp.task('js', gulp.series(function() {
+    return gulp.src(['assets/js/vendor/*.js', 'assets/js/animations/*.js', 'assets/js/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('theme.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+
+        .pipe(gulp.dest('.'));
+}))
+
+gulp.task('watch', gulp.series(function() {
+    gulp.watch('assets/scss/**/*.scss', gulp.series('sass'));
+
+
+}));
+
+// gulp.task('default', gulp.series('watch'));
+
+
+// Remove pre-existing content from output and test folders
+gulp.task('clean:dist', function() {
+
+});
+
+
 
 var banner = {
+    full: '/*!\n' +
+        ' * <%= package.name %> v<%= package.version %>: <%= package.description %>\n' +
+        ' * (c) ' + new Date().getFullYear() + ' <%= package.author.name %>\n' +
+        ' * MIT License\n' +
+        ' * <%= package.repository.url %>\n' +
+        ' * Open Source Credits: <%= package.openSource.credits %>\n' +
+        ' */\n\n',
+    min: '/*!' +
+        ' <%= package.name %> v<%= package.version %>' +
+        ' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
+        ' | <%= package.license %> License' +
+        ' | <%= package.repository.url %>' +
+        ' | Open Source Credits: <%= package.openSource.credits %>' +
+        ' */\n',
     theme: '/*!\n' +
-        ' * Theme Name: <%= package.name %> - <%= package.version %>\n' +
+        ' * Theme Name: <%= package.name %>\n' +
         ' * Theme URI: <%= package.repository.url %>\n' +
         ' * GitHub Theme URI: <%= package.repository.url %>\n' +
         ' * Description: <%= package.description %>\n' +
@@ -39,77 +91,21 @@ var banner = {
 };
 
 
-gulp.task('sass', gulp.series(function(done) {
-    gulp.src(scss_path)
-        .pipe(sourcemaps.init())
-        .pipe(sass({ sourceComments: 'map' }))
-        .on('error', gutil.log)
-        .pipe(header(banner.theme, { package: package }))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("./"));
-    done();
-}));
-
-
-gulp.task('js', gulp.series(function() {
-    return gulp.src(['assets/js/vendor/*.js', 'src/js/*.js'])
-        .pipe(sourcemaps.init())
-        .pipe(concat('theme.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-
-        .pipe(gulp.dest('.'));
-}))
-
-
 
 gulp.task('serve', gulp.parallel(function() {
 
     browserSync.init({
-        proxy: proxy
+        proxy: "localhost/blank"
     });
 }, function() {
 
-    gulp.watch(scss_watch, gulp.series('sass'));
-    gulp.watch(js_watch, gulp.series('js'));
+    gulp.watch("assets/scss/**/*.scss", gulp.series('sass'));
+
+    gulp.watch("assets/js/**/*.js", gulp.series('js'));
+
     gulp.watch("**/*.php").on('change', browserSync.reload);
 
     gulp.watch("theme.js").on('change', browserSync.reload);
 }));
 
 gulp.task('default', gulp.series('sass', 'serve'));
-
-gulp.task("components", gulp.series(function(done) {
-
-    var watcher = gulp.watch('components/**/*.php', gulp.parallel('sass'));
-    watcher.on('add', function(path, stats) {
-        console.log('File ' + path + ' was added');
-        fileName = path.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, "");
-        console.log(fileName);
-        filepath = 'scss/components/_' + fileName + '.scss';
-        fs.access(filepath, (err) => {
-            if (err) {
-                // file/path is not visible to the calling process
-                console.log(err.message);
-                console.log(err.code);
-
-                fs.writeFile(filepath, "//" + fileName + " component page", (err) => {
-                    if (err) throw err;
-
-                    console.log("The file was succesfully saved!");
-                });
-
-            }
-        });
-
-    });
-
-    watcher.on('unlink', function(path) {
-        console.log('File ' + path + ' was removed');
-    });
-
-}));
