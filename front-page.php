7487429ffd67e1d78a30224d1ff1d7d09b03b599
Jack Harner <jack@harnerdesigns.com>
2019-10-26 01:57:34 -0600

    Map Link Block

--- front-page.php
+++ front-page.php
@@ -1,5 +1,13 @@
 <?php get_header();?>
 <main>
+<container class="banner purple site__intro">
+
+<h1><?= bloginfo("name"); ?></h1>
+<p>An online voting tool that identifies whether elected officials and candidates for public office are, in fact, dipshits.</p>
+
+</container>
+
+<?php get_template_part("components/maps/us_map"); ?>
 
 </main>
 <?php get_footer();?>
