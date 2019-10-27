6d3df7912b8975766a6b85aa3ee7bdecd114a180
Jack Harner <jack@harnerdesigns.com>
2019-10-26 23:08:15 -0600

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
- <footer>
+ <footer class="site__footer">
 
  	&copy; <?php echo (first_post_date() != date('Y') ? first_post_date() . " - ". date('Y') . " " : date('Y') . " "); echo bloginfo('name');?>
  </footer>
