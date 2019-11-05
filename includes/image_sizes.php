3eb998212cf8112366e758bf250fb5f148f0fd73
Jack Harner <jack@harnerdesigns.com>
2019-11-04 23:05:23 -0700

    Headshot size is more headshot shaped now

--- includes/image_sizes.php
+++ includes/image_sizes.php
@@ -3,5 +3,6 @@
 
 add_action( 'after_setup_theme', 'imrad_image_sizes' );
 function imrad_image_sizes() {
-    add_image_size( 'headshot', 500, 500, true ); 
+    add_image_size( 'headshot', 360, 500, true ); 
+    add_image_size( 'headshot-icon', 100, 100, true ); 
 }
