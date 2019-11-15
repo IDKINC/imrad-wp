1c85b364fffc78931d9730f3f346be55661c95b2
Jack Harner <jack@harnerdesigns.com>
2019-11-14 21:37:43 -0700

    Login is Fixed

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@ include __DIR__ . "/includes/image_sizes.php";
 include __DIR__ . "/includes/search_modifications.php";
 include __DIR__ . "/includes/imrad_import.php";
 
-
 include __DIR__ . "/includes/customPostTypes/people.php";
 include __DIR__ . "/includes/customPostTypes/evidence.php";
 include __DIR__ . "/includes/customPostTypes/issues.php";
@@ -17,7 +16,6 @@ include __DIR__ . "/includes/customPostTypes/districts.php";
 include __DIR__ . "/includes/customTaxonomies/job_title.php";
 include __DIR__ . "/includes/customTaxonomies/zip_codes.php";
 include __DIR__ . "/includes/customTaxonomies/party.php";
-
 
 /**
  * Proper way to enqueue scripts and styles.
@@ -33,11 +31,11 @@ function imrad_scripts()
 }
 add_action('wp_enqueue_scripts', 'imrad_scripts');
 
-function imrad_admin_scripts(){
-wp_enqueue_script('fontAwesome-backend', 'https://kit.fontawesome.com/0e434539c2.js');
+function imrad_admin_scripts()
+{
+    wp_enqueue_script('fontAwesome-backend', 'https://kit.fontawesome.com/0e434539c2.js');
 }
 add_action('admin_enqueue_scripts', 'imrad_admin_scripts');
-
 
 function imrad_theme_support()
 {
@@ -55,99 +53,115 @@ function cc_mime_types($mimes)
 }
 add_filter('upload_mimes', 'cc_mime_types');
 
-
-
-
 // Removing Default post Type
 
+add_action('admin_menu', 'remove_default_post_type');
 
-add_action( 'admin_menu', 'remove_default_post_type' );
-
-function remove_default_post_type() {
-    remove_menu_page( 'edit.php' );
+function remove_default_post_type()
+{
+    remove_menu_page('edit.php');
 }
 
-add_action( 'admin_bar_menu', 'remove_default_post_type_menu_bar', 999 );
+add_action('admin_bar_menu', 'remove_default_post_type_menu_bar', 999);
 
-function remove_default_post_type_menu_bar( $wp_admin_bar ) {
-    $wp_admin_bar->remove_node( 'new-post' );
+function remove_default_post_type_menu_bar($wp_admin_bar)
+{
+    $wp_admin_bar->remove_node('new-post');
 }
 
+add_action('wp_dashboard_setup', 'remove_draft_widget', 999);
 
-add_action( 'wp_dashboard_setup', 'remove_draft_widget', 999 );
-
-function remove_draft_widget(){
-    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+function remove_draft_widget()
+{
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
 }
-
 
 // Admin Only Dashboard
 
+function imrad_restrict_admin()
+{
 
-function imrad_restrict_admin() {
-
-    if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+    if (defined('DOING_AJAX') && DOING_AJAX) {
         return;
     }
 
-    if ( ! current_user_can( 'manage_options' ) ) {
-        wp_redirect( home_url() );
+    if (!current_user_can('manage_options')) {
+        wp_redirect(home_url());
         exit;
     }
 }
 
-
 add_action('admin_init', 'imrad_restrict_admin');
 
-function redirect_login_page() {
-	$login_page  = home_url('/login/');
-	$page_viewed = basename($_SERVER['REQUEST_URI']);
+function admin_default_page($redirect_to, $request, $user)
+{
 
-	if($page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
-		wp_redirect($login_page);
-		exit;
-	}
+    if (isset($user->roles) && is_array($user->roles)) {
+        //check for admins
+        if (in_array('administrator', $user->roles)) {
+            // redirect them to the default place
+            return admin_url();
+        } else {
+            return home_url();
+        }
+    } else {
+        return $redirect_to;
+    }
+    //todo check user role and redirect accordingly.
 }
-add_action('init','redirect_login_page');
+
+add_filter('login_redirect', 'admin_default_page', 10, 3);
+
+function redirect_login_page()
+{
+    $login_page = home_url('/login/');
+    $page_viewed = $_SERVER['REQUEST_URI'];
+    
+    if ($page_viewed == "/wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+        wp_redirect($login_page);
+        exit;
+    }
+    if ($page_viewd == "/wp-login.php?loggedout=true" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+
+        wp_redirect(home_url());
+        exit;
+
+    }
+}
+add_action('init', 'redirect_login_page');
 
 /* Where to go if a login failed */
-function custom_login_failed() {
-	$login_page  = home_url('/login/');
-	wp_redirect($login_page . '?login=failed');
-	exit;
+function custom_login_failed()
+{
+    $login_page = home_url('/login/');
+    wp_redirect($login_page . '?login=failed');
+    exit;
 }
-add_action('wp_login_failed', 'custom_login_failed');
+// add_action('wp_login_failed', 'custom_login_failed');
 
 /* Where to go if any of the fields were empty */
-function verify_user_pass($user, $username, $password) {
-	$login_page  = home_url('/login/');
-	if($username == "" || $password == "") {
-		wp_redirect($login_page . "?login=empty");
-		exit;
-	}
+function verify_user_pass($user, $username, $password)
+{
+    $login_page = home_url('/login/');
+    if ($username == "" || $password == "") {
+        wp_redirect($login_page . "?login=empty");
+        exit;
+    }
 }
-add_filter('authenticate', 'verify_user_pass', 1, 3);
+// add_filter('authenticate', 'verify_user_pass', 1, 3);
 
 /* What to do on logout */
-function logout_redirect() {
-	$login_page  = home_url('/login/');
-	wp_redirect($login_page . "?login=false");
-	exit;
+function logout_redirect()
+{
+    $login_page = home_url('/');
+    wp_redirect($login_page . "?login=false");
+    exit;
 }
 add_action('wp_logout','logout_redirect');
 
-
-function admin_default_page() {
-	return home_url();
-
-	//todo check user role and redirect accordingly.
-  }
-  
-  add_filter('login_redirect', 'admin_default_page');
-
-add_filter( 'show_admin_bar', function( $show ) {
-	if ( current_user_can( 'subscriber' ) ) {
-		return false;
-	}
-	return $show;
-} );
+add_filter('show_admin_bar', function ($show) {
+    if (current_user_can('subscriber')) {
+        return false;
+    }
+    return $show;
+});
--- page_templates/login.php
+++ page_templates/login.php
@@ -5,7 +5,39 @@ Template Name: Login
 
 ?>
 
-<?php get_header();?>
+<?php get_header();
+
+if($_POST) 
+{ 
+
+global $wpdb; 
+
+//We shall SQL escape all inputs 
+$username = $wpdb->escape($_REQUEST['username']); 
+$password = $wpdb->escape($_REQUEST['password']); 
+$remember = $wpdb->escape($_REQUEST['rememberme']); 
+
+if($remember) $remember = "true"; 
+else $remember = "false"; 
+
+$login_data = array(); 
+$login_data['user_login'] = $username; 
+$login_data['user_password'] = $password; 
+$login_data['remember'] = $remember; 
+
+$user_verify = wp_signon( $login_data, false ); 
+
+if ( is_wp_error($user_verify) ) 
+{ 
+echo '<span class="mine">Invlaid Login Details</span>'; 
+} else { 
+echo "<script type='text/javascript'>window.location.href='". home_url() ."'</script>"; 
+exit(); 
+} 
+
+} else 
+{  }
+?>
 
 <main>
 <container class="banner login">
@@ -14,9 +46,28 @@ Template Name: Login
 
 <?php 
 
-$loginArgs = array();
+$loginArgs = array('redirect' => home_url());
 
 wp_login_form($loginArgs); ?>
+
+<hr />
+
+<a href="#" class="need">Need An Account?</a>
+
+</section>
+
+<div class="separator vertical">OR</div>
+
+
+
+<section class="login__card">
+<h1>Create An Account</h1>
+
+<?php 
+
+$loginArgs = array('redirect' => home_url());
+
+wp_register_form($loginArgs); ?>
 
 <hr />
 
@@ -28,3 +79,4 @@ wp_login_form($loginArgs); ?>
 	</main>
 
 <?php get_footer();?>
+
